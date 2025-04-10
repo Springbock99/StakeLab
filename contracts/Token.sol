58235abae7f7819c9873f79f2670f1412725f117
Springbock99 <daneelkent@gmail.com>
2025-04-10 14:56:26 +0200

    adding the base of the project, with soldeer and foundry for testing

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract Token is ERC20 {
-    constructor(uint initialBalance) ERC20("FarmToken", "FTK") {
+import "@openzeppelin-contracts-5.0.2/token/ERC20/ERC20.sol";
+import "@openzeppelin-contracts-5.0.2/access/Ownable.sol";
+
+contract Token is Ownable, ERC20 {
+    uint256 intitialBalanc = 100000000000000;
+    constructor(
+        uint initialBalance
+    ) ERC20("FarmToken", "FTK") Ownable(msg.sender) {
         _mint(msg.sender, initialBalance);
     }
 }

4de725859c0c48d9d5eafa43087ecfeb7b6bca93
Springbock99 <daneelkent@gmail.com>
2025-04-11 08:40:53 +0200

    adding my erc20 token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,10 +5,7 @@ import "@openzeppelin-contracts-5.0.2/token/ERC20/ERC20.sol";
 import "@openzeppelin-contracts-5.0.2/access/Ownable.sol";
 
 contract Token is Ownable, ERC20 {
-    uint256 intitialBalanc = 100000000000000;
-    constructor(
-        uint initialBalance
-    ) ERC20("FarmToken", "FTK") Ownable(msg.sender) {
-        _mint(msg.sender, initialBalance);
+    constructor(uint amount) ERC20("FarmToken", "FTK") Ownable(msg.sender) {
+        _mint(msg.sender, amount * 1e18);
     }
 }
